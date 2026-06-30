901f06469d7791cf0a6063f4f61e33a339a5aea7
weisswurstmann <fabianwolf.mechenhard@gmx.de>
2026-06-30 10:13:11 +0200

    add value

--- src/value.zig
+++ src/value.zig
@@ -1,2 +1,14 @@
 const std = @import("std");
 
+pub const Word = i64;
+pub const Flags = struct { zero: bool = false, sign: bool = false };
+pub const Regs = struct {
+    r: [16]Word = [_]Word{0} ** 16,
+    pub fn get(self: Regs, i: u8) Word {
+        return self.r[i];
+    }
+    pub fn set(self: *Regs, i: u8, v: Word) void {
+        self.r[i] = v;
+    }
+};
+
