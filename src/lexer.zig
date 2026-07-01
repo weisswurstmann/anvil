d309c297909803b8e21ab50fe471f6817aa6c0a9
weisswurstmann <fabianwolf.mechenhard@gmx.de>
2026-07-01 13:12:08 +0200

    wip lexer

--- src/lexer.zig
+++ src/lexer.zig
@@ -57,3 +57,4 @@ pub fn tokenize(alloc: std.mem.Allocator, src: []const u8) ![]Token {
             continue;
         }
 
+// Newline if (c == '\n') { try list.append(alloc, Token{ .kind = .newline, .text = src[i .. i + 1], .line = lin }); line += 1; i += 1; continue; }
